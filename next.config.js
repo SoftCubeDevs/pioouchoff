53669b1a7aced2740db1c640838e6da0dccfd2d2
ErickG98 <erickutngarcia@gmail.com>
2024-01-03 22:17:58 -0600

    estaticos

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+    output:'export',
+    //sassOptions: {
+    //  includePaths: [path.join(__dirname, "css")],
+    //},
+    //trailingSlash: true,
+    //devIndicators: {
+    //  buildActivity: true,
+    //},
+    //eslint: {
+    //  ignoreDuringBuilds: true,
+    //},
+  };
