b7186d99dc0b325039ab43c8b2621a4482331184
Bryan <bryan@MacBook-Air-de-Bryan.local>
2024-05-31 17:13:34 -0600

    change messages on btn whatsapp

--- components/Btnwp.js
+++ components/Btnwp.js
@@ -5,7 +5,7 @@ import { FaPhone } from "react-icons/fa";
 const Btnwp = () => {
   return (
     <>
-      <div className='btn-wp btn-fixed'><a href='https://wa.me/5215551004343'><FaWhatsapp /></a></div>
+      <div className='btn-wp btn-fixed'><a href='https://api.whatsapp.com/send?phone=5215551004343&text=Hola%20Sin%20Piojos!!%20Me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n.%20Pueden%20atenderme%20por%20favor?'><FaWhatsapp /></a></div>
       <div className='btn-pn btn-fixed'><a href='tel:5551004343'><FaPhone /></a></div>
     </>
     
